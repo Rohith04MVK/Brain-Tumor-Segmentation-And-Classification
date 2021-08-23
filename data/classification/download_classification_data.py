7794815999b33ffbaad1cf0cf8a3863ddf05e1b2
Rohith04MVK <007rohitjj04@gmail.com>
2021-08-23 17:29:37 +0530

    Added method to download the datasers

---
+++ data/classification/download_classification_data.py
@@ -0,0 +1,5 @@
+import opendatasets as open
+
+data_url = "https://www.kaggle.com/sartajbhuvaji/brain-tumor-classification-mri"
+
+open.download(data_url)
---
+++ data/segmentation/download_segmentation_data.py
@@ -0,0 +1,5 @@
+import opendatasets as open
+
+data_url = "https://www.kaggle.com/mateuszbuda/lgg-mri-segmentation/"
+
+open.download(data_url)
