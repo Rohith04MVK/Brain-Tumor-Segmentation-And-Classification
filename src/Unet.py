112f6bd723ec0995a7afbbab001e433e44ac0bbe
Rohith04MVK <007rohitjj04@gmail.com>
2021-08-25 15:27:01 +0530

    Adding the unet for segmentation

---
+++ src/Unet.py
@@ -0,0 +1,85 @@
+import tensorflow as tf
+from tensorflow.keras import Input
+from tensorflow.keras.models import Model, load_model, save_model
+from tensorflow.keras.layers import (
+    Input,
+    Activation,
+    BatchNormalization,
+    Dropout,
+    Lambda,
+    Conv2D,
+    Conv2DTranspose,
+    MaxPooling2D,
+    concatenate,
+)
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.utils import plot_model
+
+from tensorflow.keras import backend as K
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+
+im_width = 256
+im_height = 256
+
+
+def adjust_data(img, mask) -> tuple:
+    img = img / 255
+    mask = mask / 255
+    mask[mask > 0.5] = 1
+    mask[mask <= 0.5] = 0
+
+    return (img, mask)
+
+
+def unet_data_generator(
+    data_frame,
+    batch_size,
+    aug_dict,
+    image_color_mode="rgb",
+    mask_color_mode="grayscale",
+    image_save_prefix="image",
+    mask_save_prefix="mask",
+    save_to_dir=None,
+    target_size=(256, 256),
+    seed=1,
+):
+    """
+    can generate image and mask at the same time use the same seed for
+    image_datagen and mask_datagen to ensure the transformation for image
+    and mask is the same if you want to visualize the results of generator,
+    set save_to_dir = "your path"
+    """
+    image_datagen = ImageDataGenerator(**aug_dict)
+    mask_datagen = ImageDataGenerator(**aug_dict)
+
+    image_generator = image_datagen.flow_from_dataframe(
+        data_frame,
+        x_col="filename",
+        class_mode=None,
+        color_mode=image_color_mode,
+        target_size=target_size,
+        batch_size=batch_size,
+        save_to_dir=save_to_dir,
+        save_prefix=image_save_prefix,
+        seed=seed,
+    )
+
+    mask_generator = mask_datagen.flow_from_dataframe(
+        data_frame,
+        x_col="mask",
+        class_mode=None,
+        color_mode=mask_color_mode,
+        target_size=target_size,
+        batch_size=batch_size,
+        save_to_dir=save_to_dir,
+        save_prefix=mask_save_prefix,
+        seed=seed,
+    )
+
+    train_gen = zip(image_generator, mask_generator)
+
+    for (img, mask) in train_gen:
+        img, mask = adjust_data(img, mask)
+        yield (img, mask)
