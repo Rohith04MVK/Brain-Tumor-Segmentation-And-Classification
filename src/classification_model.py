a8014d7045fd97f4f5ef89de6118502e0c6c51e3
Rohith04MVK <007rohitjj04@gmail.com>
2021-08-26 17:02:07 +0530

    Added documentation to classification model

--- src/classification_model.py
+++ src/classification_model.py
@@ -3,7 +3,19 @@ from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Dropout
 from tensorflow.keras.models import Model
 
 
-def classification_model(input_size):
+def classification_model(input_size) -> Model:
+    """
+    The model used to classify the type of tumor.
+    This function uses the `EfficientNetB0` for transfer learning.
+
+    Parameters
+    ----------
+    input_size - The size of the input data
+
+    Return
+    ------
+    model - tensorflow.keras.models.Model with the `EfficientNetB0` with additional layers for the classification
+    """
     effnet = EfficientNetB0(
         weights="imagenet", include_top=False, input_shape=input_size
     )
