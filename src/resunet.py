c80bb0653b2aa2c30d0136b845df7e54cce02ca5
Rohith04MVK <007rohitjj04@gmail.com>
2021-08-31 19:38:37 +0530

    Added ResUNet

--- src/resunet.py
+++ src/resunet.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
 from tensorflow.keras.layers import *
+from tensorflow.keras.models import Model
 
 
 def resblock(X, filters):
@@ -45,7 +45,84 @@ def resblock(X, filters):
 
 def upsample_concat(x, skip):
     """
-    funtion for upsampling image
+    Funtion for upsampling image
+
+    Parameters
+    ----------
+    X - Input
+
+    skip - The layer to skip to
     """
     X = UpSampling2D((2, 2))(x)
     return Concatenate()([X, skip])
+
+
+def resunet(input_shape):
+    """
+    ResUNet
+    -------
+    A semantic segmentation model inspired by the deep residual learning and UNet.
+    An architecture that take advantages from both(Residual and UNet) models.
+
+    Paper: https://arxiv.org/pdf/1711.10684.pdf
+
+    Parameter
+    ---------
+    input_shape: tuple
+        The shape of the data
+
+    Return
+    ------
+    model: tensorflow.keras.models.Model
+        The ResUNet
+    """
+    X_input = Input(input_shape)  # iniating tensor of input shape
+
+    # Stage 1
+    conv_1 = Conv2D(
+        16, 3, activation="relu", padding="same", kernel_initializer="he_normal"
+    )(X_input)
+    conv_1 = BatchNormalization()(conv_1)
+    conv_1 = Conv2D(
+        16, 3, activation="relu", padding="same", kernel_initializer="he_normal"
+    )(conv_1)
+    conv_1 = BatchNormalization()(conv_1)
+    pool_1 = MaxPool2D((2, 2))(conv_1)
+
+    # stage 2
+    conv_2 = resblock(pool_1, 32)
+    pool_2 = MaxPool2D((2, 2))(conv_2)
+
+    # Stage 3
+    conv_3 = resblock(pool_2, 64)
+    pool_3 = MaxPool2D((2, 2))(conv_3)
+
+    # Stage 4
+    conv_4 = resblock(pool_3, 128)
+    pool_4 = MaxPool2D((2, 2))(conv_4)
+
+    # Stage 5 (bottle neck)
+    conv_5 = resblock(pool_4, 256)
+
+    # Upsample Stage 1
+    up_1 = upsample_concat(conv_5, conv_4)
+    up_1 = resblock(up_1, 128)
+
+    # Upsample Stage 2
+    up_2 = upsample_concat(up_1, conv_3)
+    up_2 = resblock(up_2, 64)
+
+    # Upsample Stage 3
+    up_3 = upsample_concat(up_2, conv_2)
+    up_3 = resblock(up_3, 32)
+
+    # Upsample Stage 4
+    up_4 = upsample_concat(up_3, conv_1)
+    up_4 = resblock(up_4, 16)
+
+    # final output
+    out = Conv2D(
+        1, (1, 1), kernel_initializer="he_normal", padding="same", activation="sigmoid"
+    )(up_4)
+
+    return Model(X_input, out)
