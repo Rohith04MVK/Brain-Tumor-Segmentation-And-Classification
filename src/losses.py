ad3c38cead7cd865ab8e67fb3b51c76ebc4eba98
Rohith04MVK <007rohitjj04@gmail.com>
2021-09-01 19:18:28 +0530

    Added documentation from all the loss functions

--- src/losses.py
+++ src/losses.py
@@ -6,7 +6,15 @@ smooth = 1
 
 
 def tversky(y_true, y_pred):
+    """
+    The Tversky Index (TI) is a asymmetric similarity measure that is a
+    generalisation of the dice coefficient and the Jaccard index.
 
+    Parameters
+    ----------
+    y_ture - The true values oe the labels
+    y_pred - The predicted labels
+    """
     y_true_pos = K.flatten(y_true)
     y_pred_pos = K.flatten(y_pred)
     true_pos = K.sum(y_true_pos * y_pred_pos)
@@ -19,6 +27,16 @@ def tversky(y_true, y_pred):
 
 
 def focal_tversky(y_true, y_pred):
+    """
+    The Focal Tversky Loss (FTL) is a generalisation of the tversky loss.
+    The non-linear nature of the loss gives you control over how the loss behaves at different values of the tversky index obtained.
+    γ is a parameter that controls the non-linearity of the loss.
+
+    Parameters
+    ----------
+    y_ture - The true values oe the labels
+    y_pred - The predicted labels
+    """
     y_true = tf.cast(y_true, tf.float32)
     y_pred = tf.cast(y_pred, tf.float32)
 
@@ -28,4 +46,12 @@ def focal_tversky(y_true, y_pred):
 
 
 def tversky_loss(y_true, y_pred):
+    """
+    It's just the Tversky Index subtracted from the 1
+
+    Parameters
+    ----------
+    y_ture - The true values oe the labels
+    y_pred - The predicted labels
+    """
     return 1 - tversky(y_true, y_pred)
